276e29e19dbe52dd0a0db8c04d269895c3f96a48
juan <juansoussev@gmail.com>
2023-07-13 19:24:20 -0300

    nova aula

--- start/src/App.js
+++ start/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import Primeiro from './components/Primeiro';
+import {Comp2, Comp3} from './components/Multi';
+
 export default function App() {
   return (
     <>
-      <Text>asd</Text>
+      <Text>asdasdasdasdadadasdadsd</Text>
+      <Primeiro />
+      <Comp3 />
+      <Comp2 />
     </>
   );
 }
---
+++ start/src/components/Multi.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default function Comp1() {
+  return (
+    <>
+      <Text>Componente 01</Text>
+    </>
+  );
+}
+
+export function Comp2() {
+  return (
+    <>
+      <Text>O comp. 02</Text>
+    </>
+  );
+}
+
+export function Comp3() {
+  return (
+    <>
+      <Text>Componnete 3 ae</Text>
+    </>
+  );
+}
