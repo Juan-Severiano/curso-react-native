0a7902c450f29514d474b4dcbba1895fcde9192e
juan <juansoussev@gmail.com>
2023-07-13 19:35:02 -0300

    aula sobre StyleSheets

--- start/src/App.js
+++ start/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 
 import Primeiro from './components/Primeiro';
 import {Comp2, Comp3} from './components/Multi';
 
 export default function App() {
   return (
-    <>
+    <View style={style.App}>
       <Text>asdasdasdasdadadasdadsd</Text>
       <Primeiro />
       <Comp3 />
       <Comp2 />
-    </>
+    </View>
   );
 }
+
+const style = StyleSheet.create({
+  App: {
+    flex: 1,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- start/src/components/Primeiro.js
+++ start/src/components/Primeiro.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text} from 'react-native';
+import Style from './style';
 
 export default function Primeiro() {
-  return <Text>rapaiz</Text>;
+  return <Text style={Style.txtG}>rapaiz</Text>;
 }
