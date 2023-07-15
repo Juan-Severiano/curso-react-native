a42e38245e59d880631648b86130a3cd1cef9e4e
juan <juansoussev@gmail.com>
2023-07-15 10:45:03 -0300

    mais sobre destructor

--- start/src/components/Aleatorio.js
+++ start/src/components/Aleatorio.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text } from 'react-native';
 import Style from './style';
 
-export default props => {
-  const res = Math.floor(Math.random() * (props.max - props.min) + props.min);
+export default ({ min, max }) => {
+  const res = Math.floor(Math.random() * (max - min) + min);
   return (
     <Text style={Style.txtG}>
-      O valor aleatorio entre {props.min} e {props.max} é {res}
+      O valor aleatorio entre {min} e {max} é {res}
     </Text>
   );
 };
--- start/src/components/MinMax.js
+++ start/src/components/MinMax.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text } from 'react-native';
 import Style from './style';
 
-export default param => (
+export default (param) => (
   <Text style={Style.txtG}>
     {param.max} > {param.min}
   </Text>
