b1f0805fb56b5d57c821bbdb9b70a2507be8da8b
juan <juansoussev@gmail.com>
2023-07-23 18:14:13 -0300

    inciando o exercicio da mega sena

--- start/src/components/mega/Mega.js
+++ start/src/components/mega/Mega.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Text, TextInput } from 'react-native';
+import { Button, Text, TextInput } from 'react-native';
 import Style from '../style';
 
 export default class Mega extends React.Component {
   state = {
     qtdeNumbers: this.props.qtdeNumbers,
+    numbers: [],
   };
 
-  handleQtdeNumber(qtde) {
+  handleQtdeNumber = (qtde) => {
     this.setState({ qtdeNumbers: qtde });
+  };
+
+  gerarNumero = (nums) => {
+    const novo = parseInt(Math.random() * 60) + 1;
+    return nums.includes(novo) ? this.gerarNumero(nums) : novo;
+  };
+
+  gerarNumeros() {
+    const numeros = Array(this.state.qtdeNumbers)
+      .fill()
+      .reduce((n) => [...n, this.gerarNumero(n)], []);
+    this.setState({ numbers: numeros });
   }
 
   render() {
@@ -21,8 +34,10 @@ export default class Mega extends React.Component {
           keyboardType="numeric"
           placeholder="Bota esse capeta ai"
           value={this.state.qtdeNumbers}
-          onChangeText={(qtde) => this.handleQtdeNumber(qtde)}
+          onChangeText={this.handleQtdeNumber}
         />
+        <Button title="Gerar" onPress={this.gerarNumeros} />
+        <Text>{this.state.numbers.join(',')}</Text>
       </>
     );
   }
