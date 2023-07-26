2397f5a3f73c4bc3ab86817aa66da511f16ce1da
juan <juansoussev@gmail.com>
2023-07-26 14:42:10 -0300

    Lógica de navegação concluida

--- rncrud/src/App.js
+++ rncrud/src/App.js
@@ -5,22 +5,51 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import UserList from './views/UserList';
 import UserForm from './views/UserForm';
+import { Button, Icon } from 'react-native-elements';
 
 const Stack = createNativeStackNavigator()
 
 export default props => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='UserList'>
-        <Stack.Screen
-          name="UserList"
-          component={UserList}
-        />
-        <Stack.Screen
-          name="UserForm"
-          component={UserForm}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='UserList'
+          screenOptions={screenOptions}>
+          <Stack.Screen
+            name="UserList"
+            component={UserList}
+            options={( {navigation} ) => {
+              return {
+                title: 'Lista de Users',
+                headerRight: () => (
+                  <Button 
+                  onPress={() => navigation.navigate('UserForm')}
+                    type='clear'
+                    icon={<Icon name='add' size={25} color='#fff'/>}
+                  />
+                )
+              }
+            }}
+          />
+          <Stack.Screen
+            name="UserForm"
+            component={UserForm}
+            options={{
+              title: 'Formulário de Usuários'
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   )
+}
+
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  }
 }
