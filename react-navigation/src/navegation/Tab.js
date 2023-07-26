97881da014cbcc9f2303a9403133aaeca3e2831e
juan <juansoussev@gmail.com>
2023-07-26 12:25:55 -0300

    Navigation Tab

---
+++ react-navigation/src/navegation/Tab.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import ScreenA from '../views/ScreenA'
+import ScreenB from '../views/ScreenB'
+import ScreenC from '../views/ScreenC'
+
+const Tab = createBottomTabNavigator()
+
+export default props => (
+  <Tab.Navigator tabBarOptions={{
+    activeTintColor: 'red',
+    inativeTintColor: 'blue',
+    labelStyle: { fontSize: 30 }
+  }}>
+    <Tab.Screen name='ScreenA' component={ScreenA} />
+    <Tab.Screen name='ScreenB' component={ScreenB} />
+    <Tab.Screen name='ScreenC' component={ScreenC} />
+  </Tab.Navigator>
+)
--- react-navigation/src/navegation/index.js
+++ react-navigation/src/navegation/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Text, SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import Stack from './Stack'
+import Tab from './Tab'
+// import Stack from './Stack'
 
 export default props => {
   return (
     <SafeAreaView style={{ flex: 1, marginTop: 30 }}>
       <NavigationContainer>
-        <Stack />
+      <Tab />
+        {/* <Stack /> */}
       </NavigationContainer>
     </SafeAreaView>
   )
