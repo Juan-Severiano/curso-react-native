c683c55690b818238c729ba94b09d43cbe6b885c
juan <juansoussev@gmail.com>
2023-07-26 13:28:51 -0300

    Navigation tab Usando Icons

--- react-navigation/src/navegation/Tab.js
+++ react-navigation/src/navegation/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from '@expo/vector-icons/Ionicons'
 import ScreenA from '../views/ScreenA'
 import ScreenB from '../views/ScreenB'
 import ScreenC from '../views/ScreenC'
@@ -7,11 +8,29 @@ import ScreenC from '../views/ScreenC'
 const Tab = createBottomTabNavigator()
 
 export default props => (
-  <Tab.Navigator tabBarOptions={{
-    activeTintColor: 'red',
-    inativeTintColor: 'blue',
-    labelStyle: { fontSize: 30 }
-  }}>
+  <Tab.Navigator screenOptions={({ route }) => ({
+    tabBarIcon: ({ focused, color, size }) => {
+      let iconName;
+      switch (route.name) {
+        case 'ScreenA':
+          iconName = focused
+            ? 'ios-information-circle'
+            : 'ios-information-circle-outline';
+          break
+        case 'ScreenB':
+          iconName = focused
+            ? 'ios-information-circle'
+            : 'ios-information-circle-outline';
+          break
+        case 'ScreenC':
+          iconName = focused ? 'ios-list' : 'ios-list-outline';
+          break
+      }
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray',
+  })}>
     <Tab.Screen name='ScreenA' component={ScreenA} />
     <Tab.Screen name='ScreenB' component={ScreenB} />
     <Tab.Screen name='ScreenC' component={ScreenC} />
--- react-navigation/src/navegation/index.js
+++ react-navigation/src/navegation/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Text, SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-// import Tab from './Tab'
-import Drawer from './Drawer'
+import Tab from './Tab'
+// import Drawer from './Drawer'
 // import Stack from './Stack'
 
 export default props => {
   return (
     <SafeAreaView style={{ flex: 1, marginTop: 30 }}>
       <NavigationContainer>
-        <Drawer />
-        {/* <Tab /> */}
+        {/* <Drawer /> */}
+        <Tab />
         {/* <Stack /> */}
       </NavigationContainer>
     </SafeAreaView>
