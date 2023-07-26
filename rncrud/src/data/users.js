8340286a2850984b956a4f7140c655ad172ba298
juan <juansoussev@gmail.com>
2023-07-26 15:23:35 -0300

    done

--- rncrud/src/data/users.js
+++ rncrud/src/data/users.js
@@ -23,40 +23,40 @@ export default [
     email: 'emma.watson@example.com',
     avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_1280.png'
   },
-  {
-    id: 5,
-    name: 'Michael Scott',
-    email: 'michael.scott@example.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/10/04/amusing-1300541_1280.png'
-  },
-  {
-    id: 6,
-    name: 'Alice Cooper',
-    email: 'alice.cooper@example.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295431_1280.png'
-  },
-  {
-    id: 7,
-    name: 'Peter Parker',
-    email: 'peter.parker@example.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295432_1280.png'
-  },
-  {
-    id: 8,
-    name: 'Linda Johnson',
-    email: 'linda.johnson@example.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/57/avatar-1295429_1280.png'
-  },
-  {
-    id: 9,
-    name: 'Chris Hemsworth',
-    email: 'chris.hemsworth@example.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300583_1280.png'
-  },
-  {
-    id: 10,
-    name: 'Jessica Williams',
-    email: 'jessica.williams@example.com',
-    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/10/04/avatar-1300540_1280.png'
-  }
+  // {
+  //   id: 5,
+  //   name: 'Michael Scott',
+  //   email: 'michael.scott@example.com',
+  //   avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/10/04/amusing-1300541_1280.png'
+  // },
+  // {
+  //   id: 6,
+  //   name: 'Alice Cooper',
+  //   email: 'alice.cooper@example.com',
+  //   avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295431_1280.png'
+  // },
+  // {
+  //   id: 7,
+  //   name: 'Peter Parker',
+  //   email: 'peter.parker@example.com',
+  //   avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295432_1280.png'
+  // },
+  // {
+  //   id: 8,
+  //   name: 'Linda Johnson',
+  //   email: 'linda.johnson@example.com',
+  //   avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/57/avatar-1295429_1280.png'
+  // },
+  // {
+  //   id: 9,
+  //   name: 'Chris Hemsworth',
+  //   email: 'chris.hemsworth@example.com',
+  //   avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300583_1280.png'
+  // },
+  // {
+  //   id: 10,
+  //   name: 'Jessica Williams',
+  //   email: 'jessica.williams@example.com',
+  //   avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/10/04/avatar-1300540_1280.png'
+  // }
 ];
--- rncrud/src/views/UserList.js
+++ rncrud/src/views/UserList.js
@@ -1,8 +1,29 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
+import users from '../data/users'
+import { ListItem } from 'react-native-elements'
 
 export default props => {
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem
+        leftAvatar={{ source: { uri: user.avatarUrl }}}
+        key={user.id}
+        title={user.name}
+        subtitle={user.email}
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}
+      />
+    )
+  }
+
   return (
-    <Text>UserList</Text>
+    <View style={{ flex: 1, color: '#000'}}>
+      <FlatList 
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
   )
 }
