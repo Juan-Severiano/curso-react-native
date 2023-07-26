988a86308cc6850a33e8be2c48ce060771a6c118
juan <juansoussev@gmail.com>
2023-07-26 12:55:09 -0300

    Navigation Drawer

--- react-navigation/index.js
+++ react-navigation/index.js
@@ -1,5 +1,7 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
-
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 import Navigation from './src/navegation/index';
 
 registerRootComponent(Navigation);
+AppRegistry.registerComponent(appName, () => Navigation);
